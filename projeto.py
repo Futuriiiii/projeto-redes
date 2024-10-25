192d2230da5dca465329ee240b081c2e64f41f47
unknown <rafael.novack07@gmail.com>
2024-10-25 18:06:47 -0300

    first commit

---
+++ projeto.py
@@ -0,0 +1,145 @@
+## AUTORES
+# Carlos Rafael Torres Miranda Novack, 20210066961
+# Avani Maria de Fonseca, 20210067000
+
+import socket
+import random
+import struct
+from scapy.layers.inet import IP, UDP
+from scapy.layers.l2 import Ether
+from scapy.sendrecv import sr1
+
+SERVER_IP = '15.228.191.109'
+SERVER_PORT = 50000
+CLIENT_PORT = random.randint(1024, 65535)
+
+# Função que solicita ao usuário a escolha de um dos 4 tipos de requisição 
+def get_escolha_usuario():
+    print('\nSelecione o que deseja requisitar:')
+    print('1 - Data e hora atual')
+    print('2 - Uma mensagem motivacional para o fim do semestre')
+    print('3 - A quantidade de respostas emitidas pelo servidor até o momento')
+    print('4 - Sair')
+    escolha_usuario = input('Insira sua escolha: ')
+    
+    if  escolha_usuario == '1':
+        return 0x00
+    elif escolha_usuario == '2':
+        return 0x01
+    elif escolha_usuario == '3':
+        return 0x02
+    elif escolha_usuario == '4':
+        return None
+    else:
+        print('Escolha inválida. Por favor, tente novamente')
+        return get_escolha_usuario()
+
+# Função que vai construir a mensagem
+def build_msg(tipo_req):
+    identificador = random.randint(1, 65535)
+    message = struct.pack('!BH', tipo_req, identificador)
+    return message, identificador
+
+# Função para processar a resposta do servidor no cliente UDP
+def proc_resposta_udp(data):
+    req_res, identificador, tamanho_resposta = struct.unpack('!BHB', data[:4])
+
+    # Se o cliente solicitou a quantidade de respostas emitidas pelo servidor até o momento
+    if req_res == 0x12:
+        resposta = struct.unpack('!I', data[4:8])[0]
+    # Se o cliente solicitou qualquer outra coisa
+    else: 
+        resposta = data[4:].decode('utf-8')
+
+    return req_res, identificador, tamanho_resposta, resposta
+
+# Função para processar a resposta do servidor no cliente SCAPY
+def proc_resposta_scapy(data):
+    if data and UDP in data: # Verifica se o pacote tem camada UDP
+        payload = bytes(data[UDP].payload) # Converte o pacote Scapy em bytes
+        req_res, identificador, tamanho_resposta = struct.unpack('!BHB', payload[:4])
+
+        # Se o cliente solicitou a quantidade de respostas emitidas pelo servidor até o momento
+        if req_res == 0x12:
+            resposta = struct.unpack('!I', data.load[4:8])[0]
+        #Se o cliente solicitou qualquer outra coisa
+        else: 
+            resposta = data.load[4:].decode('utf-8')
+
+        return req_res, identificador, tamanho_resposta, resposta
+    else:
+        print("Pacote recebido não contém dados UDP válidos.")
+        return None, None, None, None
+        
+
+# Cliente UDP
+def cliente_udp():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+    while True:
+        tipo_req = get_escolha_usuario()
+        if tipo_req is None:
+            print('Saindo...')
+            break
+
+        # Construir e mandar a requisição
+        msg, identificador = build_msg(tipo_req)
+        sock.sendto(msg, (SERVER_IP, SERVER_PORT))
+        
+        # Receber a resposta do servidor
+        data, _ = sock.recvfrom(1024)
+        req_res, identificador, tamanho_resposta, resposta = proc_resposta_udp(data)
+        
+        # Mostrar a resposta
+        print(f'\nTipo da resposta (em hexadecimal): {hex(req_res)}')
+        print(f'ID: {identificador}')
+        print(f'Tamanho da resposta do servidor: {tamanho_resposta}')
+        print(f'Resposta do servidor: {resposta}')
+
+    sock.close()
+
+# Cliente SCAPY
+def cliente_scapy():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+    while True:
+        tipo_req = get_escolha_usuario()
+        if tipo_req is None:
+            print('Saindo...')
+            break
+
+        # Construir e mandar a requisição
+        msg, identificador = build_msg(tipo_req)
+        sock.sendto(msg, (SERVER_IP, SERVER_PORT))
+        
+        # Construir o UDP e receber a resposta do servidor
+        pacote = UDP(sport=CLIENT_PORT, dport=SERVER_PORT)
+        pacote = IP(dst=SERVER_IP) / pacote
+        data = sr1(pacote, timeout=2)
+
+        data.show()
+
+        if data:
+            req_res, identificador, tamanho_resposta, resposta = proc_resposta_scapy(data)
+            # Mostrar a resposta
+            print(f'\nTipo da resposta (em hexadecimal): {hex(req_res)}')
+            print(f'ID: {identificador}')
+            print(f'Tamanho da resposta do servidor: {tamanho_resposta}')
+            print(f'Resposta do servidor: {resposta}')
+
+    sock.close()
+
+# Main para o usuario escolher entre UPD e SCAPY
+if __name__ == "__main__":
+    print('Qual cliente deseja utilzar')
+    print('1 - Socket UDP')
+    print('2 - Biblioteca SCAPY')
+    escolha_main = input('Insira sua escolha: ')
+    
+    if escolha_main == '1':
+        cliente_udp()
+    elif escolha_main == '2':
+        cliente_scapy()
+    else:
+        print('Escolha invalida, encerrando o progerama...')
+    
